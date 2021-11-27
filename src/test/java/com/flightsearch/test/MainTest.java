73c1ca5f6118e8f7461ea1f74c266f76cab94bf3
Artysh <Art.kongar@yandex.ru>
2021-11-27 22:48:22 +0300

    Added tests on content

--- src/test/java/com/flightsearch/test/MainTest.java
+++ src/test/java/com/flightsearch/test/MainTest.java
@@ -1,7 +1,14 @@
 package com.flightsearch.test;
 
+import com.flightsearch.TicketSearchAmadeusService;
+import com.flightsearch.exeptions.NoTicketThisDataException;
+import com.flightsearch.exeptions.WrongInputDataException;
 import com.flightsearch.utils.DataGenerator;
+import org.json.simple.parser.ParseException;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 import static org.junit.Assert.fail;
 
@@ -9,9 +16,27 @@ public class MainTest {
 
     @Test
     public void CheckIATACode(){
-        System.out.println("Тест прогнан");
         if (!"MOW".equals(DataGenerator.getIATA("Москва"))){
             fail("IATA code not matchs");
         }
+    }
+
+    @Test
+    public void CheckTicketReturn() {
+        TicketSearchAmadeusService search = new TicketSearchAmadeusService();
+        String origin = DataGenerator.getIATA("Москва");
+        String destination = DataGenerator.getIATA("Казань");
+        try {
+            search.getOffers(origin, destination, LocalDate.now().plusDays(10), null);
+        } catch (IOException e) {
+            fail("Wrong Stream treatment or REST request failed");
+        } catch (ParseException e) {
+            fail("Wrong API parse");
+        } catch (NoTicketThisDataException e) {
+            fail("Wrong Data treatment");
+        } catch (WrongInputDataException e) {
+            fail("Wrong Data Input treatment");
+        }
+
     }
 }
