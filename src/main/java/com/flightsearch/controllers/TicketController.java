779c2b6ce8fd4d210d84d13b661d5dcca19f3279
Artysh <Art.kongar@yandex.ru>
2021-11-27 22:15:06 +0300

    Deleted imports

--- src/main/java/com/flightsearch/controllers/TicketController.java
+++ src/main/java/com/flightsearch/controllers/TicketController.java
@@ -20,8 +20,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
